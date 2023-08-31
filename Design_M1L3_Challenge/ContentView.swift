e9f026c061eee0376ae0d7efc27abf85001acf81
Tom Montgomery <montgomery3681@gmail.com>
2023-08-30 23:41:37 -0600

    - add shapes and center

--- Design_M1L3_Challenge/ContentView.swift
+++ Design_M1L3_Challenge/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // create triangle shape
+    let triangle: Path = {
+        Path({ path in
+            path.move(to: CGPoint(x: 0, y: 0))
+            path.addLines([CGPoint(x: 100, y: 0),
+                          CGPoint(x: 50, y: 100),
+                          CGPoint(x: 0, y: 0)])
+        })
+        
+        
+    }()
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack (alignment: .center) {
+         // TODO:  decrease numbers to move the chevron back to the center
+            Path({ path in
+                
+                path.move(to: CGPoint(x: 50, y: 250))
+                path.addLine(to: CGPoint(x: 100, y: 400))
+                path.addLine(to: CGPoint(x: 50, y: 550))
+                path.addLine(to: CGPoint(x: 150, y: 550))
+                path.addLine(to: CGPoint(x: 200, y: 400))
+                path.addLine(to: CGPoint(x: 150, y: 250))
+                path.addLine(to: CGPoint(x: 50, y: 250))
+                //path.closeSubpath()
+                
+            })
+            .fill(.blue)
+            .frame(width:200, height: 200)
+            //.stroke(Color.blue, style: StrokeStyle(lineWidth: 2))
+            ZStack {
+                triangle
+                    .fill(Color.yellow)
+                    .padding(.top, 15)
+                triangle
+                    .fill(Color.yellow)
+                    .rotationEffect(Angle(degrees: 180))
+                    .padding(.bottom, 15)
+            }
+            .frame(width: 100, height: 100)
+            
         }
-        .padding()
     }
 }
 
